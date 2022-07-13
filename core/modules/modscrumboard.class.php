1b5a829a80f974cad66f30af9bc2d6c7d31bcc06
John BOTELLA <john.botella@atm-consulting.fr>
2022-07-13 12:41:01 +0200

    Update core/modules/modscrumboard.class.php

--- core/modules/modscrumboard.class.php
+++ core/modules/modscrumboard.class.php
@@ -53,7 +53,7 @@ class modscrumboard extends DolibarrModules
 
 		// Family can be 'crm','financial','hr','projects','products','ecm','technic','other'
 		// It is used to group modules in module setup page
-		$this->family = 'ATM Consulting - BTP';
+		$this->family = 'projects';
 		// Module label (no space allowed)
 		// used if translation string 'ModuleXXXName' not found
 		// (where XXX is value of numeric property 'numero' of module)
