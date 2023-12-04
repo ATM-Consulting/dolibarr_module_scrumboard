c6ec4e92fea81f9492d9d13e8d70bf9beef154a8
Saami PERDRIX - ATM Consulting <saami.perdrix@atm-consulting.fr>
2023-12-04 10:44:24 +0100

    refacto backport

--- class/actions_scrumboard.class.php
+++ class/actions_scrumboard.class.php
@@ -1,10 +1,8 @@
 <?php
 
-$commonhookactionsclassfileDolibarr     = DOL_DOCUMENT_ROOT.'/core/class/commonhookactions.class.php';
-$commonhookactionsclassfileBackport     = __DIR__.'/../backport/v19/core/class/commonhookactions.class.php';
-$commonhookactionsclassfileToUse        = (file_exists($commonhookactionsclassfileDolibarr)) ? $commonhookactionsclassfileDolibarr : $commonhookactionsclassfileBackport;
-require_once $commonhookactionsclassfileToUse;
-class ActionsScrumboard extends CommonHookActions
+require_once __DIR__ . '/../backport/v19/core/class/commonhookactions.class.php';
+
+class ActionsScrumboard extends scrumboard\RetroCompatCommonHookActions
 {
      /** Overloading the doActions function : replacing the parent's function with the one below
       *  @param      parameters  meta datas of the hook (context, etc...)
