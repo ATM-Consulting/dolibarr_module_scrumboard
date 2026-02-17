df01c8a3361a364aed94cdcc4b6102c8623a7910
ATM-Lucas <lucas.mantegari@atm-consulting.fr>
2026-02-17 12:20:28 +0100

    Scrum project view dictionary conf

--- class/scrumboard.class.php
+++ class/scrumboard.class.php
@@ -85,6 +85,24 @@ class ScrumboardColumn extends TObjetStd
 	}
 
 	/**
+	 * Build default scrum columns as objects.
+	 *
+	 * @return array
+	 */
+	private function getDefaultColumnsForDictionaryMode()
+	{
+		$Tab = array();
+		foreach (array('backlog' => 'Ideas', 'todo' => 'toDo', 'inprogress' => 'inProgress', 'review' => 'review', 'finish' => 'finish') as $code => $label) {
+			$obj = new stdClass;
+			$obj->label = $label;
+			$obj->code = $code;
+			$Tab[] = $obj;
+		}
+
+		return $Tab;
+	}
+
+	/**
 	 * Renvoi un array contenant l'ordre des colonnes (check la conf SCRUM_ADD_BACKLOG_REVIEW_COLUMN)
 	 *
 	 * @param   bool    $force_load     Force reload from DB
@@ -98,6 +116,14 @@ class ScrumboardColumn extends TObjetStd
 			if (empty($this->TColumn) || $force_load) {
 				$PDOdb = new TPDOdb;
 				$this->LoadAllBy($PDOdb, array('active' => 1, 'entity' => $conf->entity));
+				// In multicompany setups, columns may exist only on master entity.
+				if (empty($this->TColumn) && (int) $conf->entity !== 1) {
+					$this->LoadAllBy($PDOdb, array('active' => 1, 'entity' => 1));
+				}
+				// Fallback if dictionary is empty/inactive.
+				if (empty($this->TColumn)) {
+					$this->TColumn = $this->getDefaultColumnsForDictionaryMode();
+				}
 			}
 
 			return $this->TColumn;
